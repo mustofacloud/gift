61294b91b8efa2a7767bf3b21df24ca6290dae1e
MasTopa <topaboju@gmail.com>
2025-11-03 07:38:44 +0000

    Refactor scrapeGiftData function to use slug parameter and update API URL

--- scrapeGiftData.js
+++ scrapeGiftData.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 require("dotenv").config();
 
-async function scrapeGiftData(uuid_streamer) {
-  const url = `https://api.idn.app/api/v1/gift/livestream/top-rank?uuid_streamer=${uuid_streamer}&n=1`;
+async function scrapeGiftData(slug) {
+  const url = `https://api.idn.app/api/v1/gift/livestream/${slug}/top-gifter?n=1`;
 
   try {
     const response = await axios.get(url, {
       headers: {
-        "X-API-KEY": process.env.X_API_KEY,
+        "X-API-KEY": "123f4c4e-6ce1-404d-8786-d17e46d65b5c",
       },
     });
 
